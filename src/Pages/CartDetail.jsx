3c96c87f38453b6f37c1968b024f99896c01f02c
trainerfrontend <praveen.trainer.@outlook.com>
2023-09-28 11:15:06 +0100

    remove item

--- src/Pages/CartDetail.jsx
+++ src/Pages/CartDetail.jsx
@@ -1,11 +1,20 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { remove } from "../store/CartSlice";
 
 const CartDetail = () => {
+    const dispatch = useDispatch();
     const products = useSelector(state => state.cart);
+
+    const removeItemHandler = (productId) => {
+        dispatch(remove(productId))
+    }
 
     const createProductList = () => {
         return products.map((item) => {
-            return <h1>{item.title}</h1>
+            return <div>
+                <h1>{item.title}</h1>
+                <button onClick={() => removeItemHandler(item.id)}>remove</button>
+            </div>
         })
     }
     return <div>{createProductList()}</div>
--- src/store/CartSlice.jsx
+++ src/store/CartSlice.jsx
@@ -7,12 +7,15 @@ const CartSlice = createSlice({
     initialState,
     reducers: {
         add(state, action) {
-            state.push(action.payload)
+            state.push(action.payload);
+        },
+        remove(state, action) {
+            return state.filter((item) => item.id !== action.payload)
         }
     }
 })
 
-export const { add } = CartSlice.actions
+export const { add, remove } = CartSlice.actions
 
 export default CartSlice.reducer;
 
